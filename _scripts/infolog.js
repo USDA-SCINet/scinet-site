5de938cdae41308b24139c03c292406e4d539c34
Moe Richert <moe.richert@atlas-0228.hpc.msstate.edu>
2025-06-17 12:31:30 -0500

    normaliz lf

--- _scripts/infolog.js
+++ _scripts/infolog.js
@@ -1,6 +1,6 @@
 (function() {
-    var infologs = "\x1b[36;1m scinet-site - Code dependencies were updated on 4/03/25.\x1b[0m\n" + 
-          "\x1b[36;1m scinet-site - To update your code, please use\x1b[33;1m npm run update-dep \x1b[0m\n";
+    var infologs = "\x1b[36;1m scinet-site - Code dependencies were updated on 6/17/25.\x1b[0m\n" + 
+          "\x1b[36;1m scinet-site - To update your code, please use\x1b[33;1m npm run update \x1b[0m\n";
 
     console.info(infologs);
  })();
